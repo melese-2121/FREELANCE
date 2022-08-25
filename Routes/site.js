40f8329b5e5624c641c0031b1b02b6d9be0ebb73
melese <110913050+melese-2121@users.noreply.github.com>
2022-08-25 14:39:13 +0300

    file deleted
    notification file is deleted since i can connect to database and also some file modifications are done.

--- Routes/site.js
+++ Routes/site.js
@@ -1,24 +1,42 @@
 const express = require('express');
 const router = express.Router();
 
-const { notifications } = require("../notifications");
 const sequelize = require('../connection/db.js');
 const Feedback = require('../models/Feedback.js');
+const Job = require('../models/Job');
 
+// Fetch data from database middleware
+const fetch_data = async (req, res, next) => {
+  global.data = await Job.findAll();
 
+  next();
+}
 
+// Fetch recently added data from DB
+const fetch_recent_data = async (req, res, next) => {
+  const data = await Job.findAll();
+  for(let i = 0; i < data.length/2; i++){
+    data.shift();
+  }
+  global.recent_data = data;
+  
+
+  next();
+}
 
 // Home page
-router.get("/", (req, res) => {
+router.get("/", fetch_recent_data, (req, res) => {
   res.render("home", {
-    notifications,
+    notifications: global.recent_data
   });
 });
 
 // Show page
-router.get("/posts", (req, res) => {
+router.get("/posts", fetch_data, (req, res) => {
+
+  console.log(global.data)
   res.render("posts", {
-    notifications
+    notifications: global.data
   });
 });
 
